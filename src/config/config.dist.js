8bd855453a7d918c958bcc9f32cc4ccefe9de5aa
Rafael Minguet <rminguet@gmail.com>
2024-03-19 10:39:14 +0100

    Added synchronous download for older computers

--- src/config/config.dist.js
+++ src/config/config.dist.js
@@ -1,4 +1,5 @@
 export default {
-    path   : '/path/to/folder/',    // with trailing slash
-    command: 'aws s3 cp s3://flatfiles/us_stocks_sip/minute_aggs_v1/<YYYY>/<MM>/<YYYY>-<MM>-<DD>.csv.gz <PATH><YYYY>-<MM>-<DD>-minute_aggs.csv.gz --endpoint-url https://files.polygon.io'
+    synchronous: true,
+    path       : '/path/to/folder/',    // with trailing slash
+    command    : 'aws s3 cp s3://flatfiles/us_stocks_sip/minute_aggs_v1/<YYYY>/<MM>/<YYYY>-<MM>-<DD>.csv.gz <PATH><YYYY>-<MM>-<DD>-minute_aggs.csv.gz --endpoint-url https://files.polygon.io'
 }
